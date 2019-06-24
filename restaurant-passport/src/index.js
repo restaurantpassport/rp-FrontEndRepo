4d193212d95329f928b1e3209024db58aa80ac85
Eric Fugleberg <ericfugleberg@Erics-MacBook-Pro.local>
2019-06-24 13:24:01 -0500

    Initial commit

---
+++ restaurant-passport/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
+
+const store = createStore(
+    rootReducer,
+    applyMiddleware((logger, thunk))
+);
+
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>, 
+document.getElementById('root'));
+
+
