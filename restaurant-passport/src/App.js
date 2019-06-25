0ec7667a3a47ff9c16ac590def167228104a525b
Eric Fugleberg <ericfugleberg@Erics-MacBook-Pro.local>
2019-06-24 20:49:47 -0500

    Components build out - Bootstrap working - Monday evening

--- restaurant-passport/src/App.js
+++ restaurant-passport/src/App.js
@@ -2,23 +2,20 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Login from './components/Login';
-import { NavbarBrand } from 'reactstrap';
+import { Navbar, NavbarBrand } from 'reactstrap';
 
 function App() {
   return (
     <Router>
     <div className="App">
-      <nav>
+      <Navbar>
         <NavbarBrand>Restaurant Passport</NavbarBrand>
-        <ul>
-          <li>
+        <div className='Links'>
             <Link to='/login'>Login</Link>
-          </li>
-          <li>
+          
             <Link to='/cities'>Cities</Link>
-          </li>
-        </ul>
-      </nav>
+          </div>
+      </Navbar>
         <Route path='/login' component={Login} />
         {/* <PrivateRoute exact path='/protected' component={Cities} /> */}
         {/* <PrivateRoute exact path='/protected' component={Cities} /> */}
--- restaurant-passport/src/components/Login.js
+++ restaurant-passport/src/components/Login.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Button, Col, Form, FormGroup, Label, Input } from 'reactstrap';
 
 import { login } from '../actions';
 
@@ -36,28 +36,34 @@ class Login extends React.Component {
     render() {
         return(
             <div>
-                <Form onSubmit={this.login}>
+                <Form inlineonSubmit={this.login}>
                     <FormGroup >
                         <Label for='username'>Username: </Label>
+                        <Col sm="12" md={{ size: 6, offset: 3 }}>
                     <Input
                     type='text'
                     name='username'
+                    placeholder='username'
                     value={this.state.credentials.username}
                     onChange={this.handleChange}
                     />
+                    </Col>
                     </FormGroup>
 
                     <FormGroup >
                         <Label for='password'>Password: </Label>
+                        <Col sm="12" md={{ size: 6, offset: 3 }}>
                     <Input
                     type='text'
                     name='password'
+                    placeholder='password'
                     value={this.state.credentials.password}
                     onChange={this.handleChange}
                     />
+                    </Col>
                     </FormGroup>
 
-                <Button color='primary' onClick={this.login}>Login
+                <Button color='info' onClick={this.login}>Login
                     </Button>
                 </Form>
 
